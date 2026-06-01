4dc4f3dacd00e2671685cfa827874992ea0298bc
Jordan <jordanduzabonfim@gmail.com>
2026-06-01 16:13:45 -0300

    add button to alarm

--- app.py
+++ app.py
@@ -12,43 +12,52 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 PORTA_SERIAL = 'COM5' 
 BAUD_RATE = 115200
 
-def ler_serial_arduino():
-    try:
-        ser = serial.Serial(PORTA_SERIAL, BAUD_RATE, timeout=1)
-        print(f"Conectado ao Arduino na porta {PORTA_SERIAL}!")
-        time.sleep(2)
-    except serial.SerialException as e:
-        print(f" Erro ao abrir a porta serial: {e}")
-        sys.exit(1)
-    print(" Aguardadando movimento...")
-    
+def ler_serial_arduino(ser):
+    print(" Waiting movement...")
     while True:
         try:
-            if ser.in_waiting > 0:
+            if ser and ser.in_waiting > 0:
                 data = ser.readline().decode('utf-8').strip()
-                
                 if data:
                     timestamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                    print(f"Movimento: {data} ({timestamp})")
+                    print(f"Movement: {data} ({timestamp})")
 
                     socketio.emit('motion_detected', {
-                        'mensagem': data,
+                        'message': data,
                         'timestamp': timestamp
                     })
             time.sleep(0.05) 
         except Exception as e:
-            print(f"Erro na leitura: {e}")
+            print(f"Error reading serial: {e}")
             break
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# route for receiving commands from the web 
+@socketio.on('send_command')
+def handle_comando(dados):
+    command = dados.get('command')
+    if ser and ser.is_open:
+        ser.write(command.encode('utf-8'))
+        print(f"Command sent to Serial: {command}")
+
 if __name__ == '__main__':
-    # thread para ler arduino
-    thread_serial = threading.Thread(target=ler_serial_arduino, daemon=True)
+    
+    ser = None
+    # Inicializa a conexão serial
+    try:
+        ser = serial.Serial(PORTA_SERIAL, BAUD_RATE, timeout=1)
+        print(f"Connected to Arduino on port {PORTA_SERIAL}!")
+        time.sleep(2)
+    except serial.SerialException as e:
+        print(f" Error opening serial port: {e}")
+        sys.exit(1)
+    
+    thread_serial = threading.Thread(target=ler_serial_arduino, args=(ser,), daemon=True)
     thread_serial.start()
     
-    # servidor web
-    print("Iniciando servidor web em http://localhost:5000")
+    # web server
+    print("Starting web server at http://localhost:5000")
     socketio.run(app, debug=True, use_reloader=False)
